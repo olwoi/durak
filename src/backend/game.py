ccc8eef52ac9bfb8a088cfec3787c93317d6f1f1
David Gashaw <david.gashaw@bayer.com>
2024-07-27 23:34:03 +0200

    Finished attack for now, added comments for defence

--- src/backend/game.py
+++ src/backend/game.py
@@ -33,20 +33,43 @@ class Game:
         self.state[0,0:],self.state[2,0:]=set_hand(self.state[0,0:],deck[0:7],self.state[2,0:])
         self.state[1,0:],self.state[2,0:]=set_hand(self.state[1,0:],deck[8:15],self.state[2,0:])
     
+    def send_state(self,boolean):
+        return self.state,boolean
     # 
     def user_update(self, move):
-        if self.state[5,8] and self.state[0,8]: # Player 0's turn to attack
+        if self.state[5,8] and self.state[5,10]: # Player 0's turn to attack
             legal = self.attack(move,0) # Player 0 attacks
+            if not legal:
+                self.send_state(False)
+            else: 
+                self.state[5,8] = 0
+                self.state[5,9] = 1
+                self.send_state(True)
+        elif self.state[5,9] and not self.state[5,10]: # Player 1's turn to attack
+            legal = self.attack(move,1) # Player 1 attacks
+            if not legal:
+                self.send_state(False)
+            else:  
+                self.state[5,8] = 1
+                self.state[5,9] = 0
+                self.send_state(True)
+        elif self.state[5,8] and not self.state[5,10]: # Player 0's turn to defend
+            pass
+    
+
+        
     def attack(self,move,player):
         new = self.state + move
         # Check whether player has been updated correctly
-        if self.state[5,8+player] == new[5,8+player]:
-            print("Player not updated during attack")
-            return False
         # Check that oppononents cards havent changed
         if not np.isclose(self.state[1-player,0:],new[1-player,0:]):
             print("Opponents cards changed during attack")
             return False
+        # Check that cards played on board have been updated
+        if not np.allclose(move[4,0:],move[5,0:]):
+            print("Cards played on board not updated")
+            return False
+
 
         # Isolate all the values of cards that could legally could be added to the board :LegalToAdd
         played = self.state[4,0:7] +self.state[4,8:15] + self.state[4,16:23] + self.state[4,24:31]
@@ -57,24 +80,41 @@ class Game:
         LegaltoAdd[indices + 16] = 1
         LegaltoAdd[indices + 24] = 1
         leftdefendmove = new[3,0:]
+
+
+
         # Check that the cards that are being added where in the players hand
         # ONES@new[3,0:].T Whatever cards that have been added by the player
         # self.state[4,0:]@new[3,0:].T The cards that have been played that are also in the had previously
 
-        if np.allclose(ONES@new[3,0:].T,self.state[4,0:]@new[3,0:].T):
+        if np.allclose(ONES@new[3,0:].T,self.state[player,0:]@new[3,0:].T):
             print("Player added cards that were not in their hand")
             return False
 
         # Cant add card that has not been played UNLESS it is the first move
         if np.isclose(np.sum(leftdefendmove),np.dot(LegaltoAdd,leftdefendmove)): 
+            if not np.isclose(np.sum(indices),0):
+                self.state = new
+                return True
             print("Player added cards that were not on the board")
             return False
+        
 
+        self.state = new
+        return True
+    
+    def defend(self,move,player):
+        """
+        Checks that move is a valid defence, meaning that all cards that were being attacked where defended
+        To do this following needs to be checked:
+        - The left to be defended cards (row 3 ) are 0 after move
+        - The cards that were used in defence (move row 4) were in the hand of the player (row player)
+        - The cards that were used in defence (move row 4) were on the board (row 4)
+        - The cards that were used in defence (move row 4) are no longer in the hand of the player (row player)
 
-        
-        
-        
-        return new, LegaltoAdd
+        """
+        pass
+
             
         
     
