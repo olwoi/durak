0d2063345ed6dac815c3ff4635164b5d219cead3
Oliver <148003728+olwoi@users.noreply.github.com>
2024-10-14 14:02:58 +0200

    docs: modify and add comments and todo's

--- src/frontend/durak_visualiser.py
+++ src/frontend/durak_visualiser.py
@@ -12,14 +12,14 @@ def get_suit_ordering(trump: int) -> np.ndarray:
     
     The order is determined by placing the trump suit first, followed by the rest of the suits in their default order.
 
-    Args:
+    Parameters:
         trump (int): The trump suit, which defines which suit is the most powerful. Must be an integer in the range 0-3.
 
     Returns:
         numpy.ndarray: An array representing the ordering of the suits.
     """
     assert type(trump) == int, "Trump must be an integer"
-    assert 0 <= trump < 4, "Trump must be in the range 0-3"
+    assert trump in {0, 1, 2, 3}, "Trump must be in the range 0-3"
 
     order = np.arange(4)
     order = np.delete(order, np.where(order == trump))
@@ -27,17 +27,21 @@ def get_suit_ordering(trump: int) -> np.ndarray:
 
     return order
 
+# TODO: Add function to act like get_card_list, but give array of int's
+# that may give more versatility. See convert_cards_to_strings as reference.
+
 def get_card_list(trump: int) -> np.ndarray:
+    # TODO: Modify function to return int list and use convert_cards_to_strings 
+    # afterwards in an additional function
     """
     Calculates the array of 32 cards in string representation based on the trump of the game.
 
-    Args:
+    Parameters:
         trump (int): The trump suit of the game.
 
     Returns:
         numpy.ndarray: An array of 32 cards in string representation.
     """
-
     ordering = get_suit_ordering(trump)
     ordered_suits = SUITS[ordering]
 
@@ -52,17 +56,12 @@ def convert_cards_to_strings(cards: np.ndarray, trump: int) ->  np.ndarray:
     """
     Converts a boolean array of size 32 representing the cards to be displayed into a list of card strings.
 
-    Args:
+    Parameters:
         cards (np.ndarray): A boolean array of size 32 representing the cards to be displayed.
         trump (int): An integer representing the ordering of the suits.
 
     Returns:
         List[str]: A list of card strings.
-
-    Raises:
-        AssertionError: If the type of `cards` is not a numpy array.
-        AssertionError: If the shape of `cards` is not (32,).
-        AssertionError: If the dtype of `cards` is not bool.
     """
     assert type(cards) == np.ndarray, "Cards must be a numpy array"
     assert cards.shape == (32,), "Cards must be a 1D array of length 32"
@@ -74,21 +73,17 @@ def convert_cards_to_strings(cards: np.ndarray, trump: int) ->  np.ndarray:
 
     return selected_cards
 
+# TODO: Convert convert_* functions into singular class for centralised management
 def convert_boolean_matrix(matrix: np.ndarray, trump: int) -> List[np.ndarray]:
     """
     Converts a boolean matrix of size (n, 32) representing the cards to be displayed into a list of card strings.
 
-    Args:
+    Parameters:
         matrix (np.ndarray): A boolean matrix of size (n, 32) representing the cards to be displayed.
         trump (int): An integer representing the ordering of the suits.
 
     Returns:
         List[np.ndarray]: A list of card strings.
-
-    Raises:
-        AssertionError: If the type of `matrix` is not a numpy array.
-        AssertionError: If the shape of `matrix` is not (4, 32).
-        AssertionError: If the dtype of `matrix` is not bool.
     """
     assert type(matrix) == np.ndarray, "Matrix must be a numpy array"
     assert matrix.shape[1] == 32, "Matrix must have 32 columns"
