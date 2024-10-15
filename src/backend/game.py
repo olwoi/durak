3388b8631166785f3e5b19d282b480dc3531e3cc
David Gashaw <david.gashaw@bayer.com>
2024-10-15 14:21:45 +0200

    preliminary testing

--- src/backend/game.py
+++ src/backend/game.py
@@ -13,6 +13,7 @@ def set_hand(hand, random,unplayed):
 
 class Game:
     state: np.ndarray
+    deck: np.ndarray
     """ Folowing is the structure of state, it described all the information in the game
     Row     |       Purpose          | Extra Values Index
     0       |       Player 0's Hand  | 0: Trump Ace, 1: Trump King, etc
@@ -26,16 +27,24 @@ class Game:
 
     def __init__(self):
 
-        self.state = np.zeros((32, 5))
+        self.state = np.zeros((32, 6))
         # Shuffle deck and deal cards
-        deck = np.random.permutation(52)
+        deck = np.random.permutation(32)
+        coser = np.random.randint(0,7)
+        deck = deck[np.where(deck != coser)]
+
         self.state[2,0:] = 1 # Set all cards to unplayed
-        self.state[0,0:],self.state[2,0:]=set_hand(self.state[0,0:],deck[0:7],self.state[2,0:])
-        self.state[1,0:],self.state[2,0:]=set_hand(self.state[1,0:],deck[8:15],self.state[2,0:])
+        self.state[0,0:],self.state[2,0:]=set_hand(self.state[0,0:],deck[0:6],self.state[2,0:])
+        self.state[1,0:],self.state[2,0:]=set_hand(self.state[1,0:],deck[6:12],self.state[2,0:])
+        self.deck = deck[12:]
+
+        self.state[5,coser] = 1
+
     
     def send_state(self,boolean):
         return self.state,boolean
-    # 
+
+
     def user_update(self, move):
         if self.state[5,8] and self.state[5,10]: # Player 0's turn to attack
             legal = self.attack(move,0) # Player 0 attacks
@@ -59,33 +68,58 @@ class Game:
 
         
     def attack(self,move,player):
+        """
+        Checks whether move is legal if so updates it and returns true, false otherwise
+        
+        Params:
+            move: ndarray() Changes made to state by player
+            player: 0-1 indicates what player attempted to do this move
+        
+        """
+
+
         new = self.state + move
+        
+        
         # Check whether player has been updated correctly
         # Check that oppononents cards havent changed
-        if not np.isclose(self.state[1-player,0:],new[1-player,0:]):
+        
+        if not np.isclose(self.state[1-player,0:],new[1-player,0:]): # Opponents hand is the same post and pre move
             print("Opponents cards changed during attack")
             return False
-        # Check that cards played on board have been updated
-        if not np.allclose(move[4,0:],move[5,0:]):
+        
+        #Any card that is added to left 2 defend and cards on board
+        if not np.allclose(move[3,0:],move[4,0:]): 
             print("Cards played on board not updated")
             return False
 
 
         # Isolate all the values of cards that could legally could be added to the board :LegalToAdd
         played = self.state[4,0:7] +self.state[4,8:15] + self.state[4,16:23] + self.state[4,24:31]
-        LegaltoAdd = np.zeros(32)
-        indices = np.where(played == 1)[0]
-        LegaltoAdd[indices] = 1
-        LegaltoAdd[indices + 8] = 1
-        LegaltoAdd[indices + 16] = 1
-        LegaltoAdd[indices + 24] = 1
+        played[np.where(played>=1)] = 1
+        if np.allclose(played, ZEROES): #Empty board any attack is legal
+            LegaltoAdd = np.ones(32)
+            if len(np.unique(played)!= 1): # Ensure only one denomination of card is played
+                return False
+            self.state = new
+            return True
+            
+        else:
+            LegaltoAdd = np.zeros(32)
+            
+            #Make multiple occurences of cards of same denomination be no issue
+            
+            indices = np.where(played == 1)[0]
+            LegaltoAdd[indices] = 1
+            LegaltoAdd[indices + 8] = 1
+            LegaltoAdd[indices + 16] = 1
+            LegaltoAdd[indices + 24] = 1
         leftdefendmove = new[3,0:]
-
 
 
         # Check that the cards that are being added where in the players hand
         # ONES@new[3,0:].T Whatever cards that have been added by the player
-        # self.state[4,0:]@new[3,0:].T The cards that have been played that are also in the had previously
+        # self.state[player,0:]@new[3,0:].T The cards that have been played that are also in the had previously
 
         if np.allclose(ONES@new[3,0:].T,self.state[player,0:]@new[3,0:].T):
             print("Player added cards that were not in their hand")
@@ -113,6 +147,13 @@ class Game:
         - The cards that were used in defence (move row 4) are no longer in the hand of the player (row player)
 
         """
+        new = self.state + move
+
+        if np.sum(new[3,:]) != 0: # Attempted redirect
+            if np.sum(self.state[4:0]) != 0:
+                return False # Cant redirect if you already started defending
+        
+
         pass
 
             
